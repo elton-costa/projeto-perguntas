2fb813c6855791a7219adc32b8d93da36de69bdb
elton-costa <tonrc1@hotmail.com>
2021-10-21 15:47:57 -0300

    Result screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,43 +7,57 @@ main() => runApp(PerguntaApp());
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
 
+  final _perguntas = const [
+    {
+      'texto': 'Qual é sua cor favorita?',
+      'respostas': ['Branco', 'Azul', 'Preto', 'Vermelho'],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': ['Raposa', 'Tigre', 'Elefante', 'Leão'],
+    },
+    {
+      'texto': 'Qual é seu esporte favorito?',
+      'respostas': ['Skate', 'Patins', 'Futebol', 'Bicicleta'],
+    }
+  ];
+
   void _responder() {
-    setState(() {
-      _perguntaSelecionada++;
-    });
+    if (temPergSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool get temPergSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      {
-        'texto': 'Qual é sua cor favorita?',
-        'respostas': ['Branco', 'Azul', 'Preto', 'Vermelho'],
-      },
-      {
-        'texto': 'Qual é o seu animal favorito?',
-        'respostas': ['Raposa', 'Tigre', 'Elefante', 'Leão'],
-      },
-      {
-        'texto': 'Qual é seu esporte favorito?',
-        'respostas': ['Skate', 'Patins', 'Futebol', 'Bicicleta'],
-      }
-    ];
-
-    List<String> respostas =
-        perguntas[_perguntaSelecionada].cast()['respostas'];
+    List<String> respostas = temPergSelecionada
+        ? _perguntas[_perguntaSelecionada].cast()['respostas']
+        : [];
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Perguntas'),
         ),
-        body: Column(
-          children: <Widget>[
-            Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
-            ...respostas.map((t) => Resposta(t, _responder)).toList(),
-          ],
-        ),
+        body: temPergSelecionada
+            ? Column(
+                children: <Widget>[
+                  Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
+                  ...respostas.map((t) => Resposta(t, _responder)).toList(),
+                ],
+              )
+            : const Center(
+                child: Text(
+                  'Perguntas Finalizadas',
+                  style: TextStyle(fontSize: 28),
+                ),
+              ),
       ),
     );
   }
